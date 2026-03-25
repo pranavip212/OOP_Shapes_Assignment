09e641ba2afd480f1eb6b0c609d9f80edbbbeaa5
pranavip64 <pranavip64@kprschools.ca>
2026-03-25 13:00:43 -0400

    In class work 3/25/2026

--- NewWIndowClass.py
+++ NewWIndowClass.py
@@ -2,13 +2,21 @@ from tkinter import *
 from tkinter.ttk import *
 # pretty cool feature! used a lambda function and created a small class
 #to make the code to open a new window more reusable
-class NewWindow(Toplevel):
+class Sphere(Toplevel):
     def __init__(self, master = None):
         super().__init__(master)
-        self.title("New Window")
+        self.title("SPHERES")
         self.geometry("250x150")
 
         Label(self, text="This is a new window").pack(pady=20)
 
+
+
         # deffirantie between shapes and stop it from creatin
         #more than one window if you press the button more than once
+
+class GabrielsHorn(Toplevel):
+    def __init__(self, master = None):
+        super().__init__(master)
+        self.title("SPHERES")
+        self.geometry("250x150")
--- shapes_gui.py
+++ shapes_gui.py
@@ -1,16 +1,31 @@
+import tkinter as tk
 from tkinter import *
 from tkinter.ttk import *
-from NewWIndowClass import NewWindow
+#from NewWIndowClass import NewWindow
 from tkinter import messagebox
 #from PIL import Image, ImageTk
 #citation: https://www.geeksforgeeks.org/python/open-a-new-window-with-a-button-in-python-tkinter/
+def open_new_window():
+    # Create the new Toplevel window
+    new_window = tk.Toplevel(master)
+    new_window.title("New Window with Widgets")
+    new_window.geometry("300x200") # Set the window size
+
+    # Add a Label to the new window
+    label = tk.Label(new_window, text="This is the new window!")
+    label.pack(pady=10)
+
+    # Add an Entry (input) widget to the new window
+    entry = tk.Entry(new_window, width=20)
+    entry.pack(pady=5)
 
 master = Tk()
 master.title("SHAPE PROGRAM")
 master.geometry('800x750')
 master.configure(bg="#ffd6e7")  #bg color
 Label(master, text="This is the main window").pack(pady=10)
-
+Button(master, text="Add Gloss", width=15, command=open_new_window).pack(pady=10)
+"""
 btn = Button(master, text="Open Sphere Window ")
 btn.bind("<Button>", lambda e: NewWindow(master))  # Bind the event
 
@@ -20,6 +35,8 @@ btn = Button(master, text="Open Gabriel's Horn Window ")
 btn.bind("<Button>", lambda e: NewWindow(master))  # Bind the event
 
 btn.pack(pady=10)
+"""
+
 
 # Run the Tkinter event loop
 master.mainloop()
