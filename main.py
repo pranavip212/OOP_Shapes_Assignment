3c12ecba8059127f7b77e2a6c89ce8a75b5109d0
pranavip212 <160192096+pranavip212@users.noreply.github.com>
2026-04-06 01:08:44 -0400

    better testing code

--- main.py
+++ main.py
@@ -6,8 +6,8 @@ s = Sphere("red", 5, 1, 2, 3)
 print(s.get_type())         # Sphere
 print(s.get_color())        # red
 print(s.get_radius())       # 5
-print(s.volume())           # 523.598...
-print(s.surface_area())     # 314.159...
+print(s.volume())           # 523.598
+print(s.surface_area())     # 314.159
 s.draw()
 
 
@@ -20,28 +20,36 @@ print(g.volume())       # very close to pi
 print(g.surface_area()) # inf
 g.draw()
 
+shapes = [s, g]
 
+for shape in shapes:
+    print(f"{shape.get_type()} Volume: {round(shape.volume(), 4)}")
+    print(f"{shape.get_type()} Surface Area: {round(shape.surface_area(), 4)}")
+    print('\n')
+    shape.draw()
+
+
+
+#proof of initial testing
 """s = Shape3D("Generic", "red", 1, 2, 3)
-
-
-print(s.get_type())                 # Generic
-print(s.get_color())                # red
-print(s.get_location().get_x())     # 1
-print(s.get_location().get_y())     # 2
-print(s.get_location().get_z())     # 3
+print(s.get_type())
+print(s.get_color())                
+print(s.get_location().get_x())     
+print(s.get_location().get_y())     
+print(s.get_location().get_z())     
 
 
 s.set_location(4, 5, 6)
-print(s.get_location().get_x())     # 4
-print(s.get_location().get_y())     # 5
-print(s.get_location().get_z())     # 6
+print(s.get_location().get_x())
+print(s.get_location().get_y()) 
+print(s.get_location().get_z())
 
 
 s.set_color("blue")
 s.set_type("Updated")
-print(s.get_color())                # blue
-print(s.get_type())                 # Updated
+print(s.get_color())
+print(s.get_type())
 
 
-print(s.volume())                   # None
-print(s.surface_area())          """  # None
+print(s.volume())                   
+print(s.surface_area())          """
